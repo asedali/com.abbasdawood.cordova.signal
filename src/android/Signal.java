37c0f316560859aafde6a885a80906bb6708d14c
Abbas S <abbas11.it@gmail.com>
2014-08-17 10:51:51 +0530

    First commit

---
+++ src/android/Signal.java
@@ -0,0 +1,141 @@
+package org.apache.cordova.signal;
+
+import java.util.List;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.telephony.NeighboringCellInfo;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+
+public class Signal extends CordovaPlugin {
+
+	TelephonyManager Tel;
+	GsmCellLocation cellLocation;
+	MyPhoneStateListener MyListener;
+	public static String imei;
+	public static String operator;
+	private static List<NeighboringCellInfo> NeighboringList;
+	public static int cellID;
+	public static int lac;
+
+	public Signal() {
+		
+	}
+
+	/**
+	 * Sets the context of the Command. This can then be used to do things like
+	 * get file paths associated with the Activity.
+	 *
+	 * @param cordova
+	 *            The context of the main Activity.
+	 * @param webView
+	 *            The CordovaWebView Cordova is running in.
+	 */
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+		super.initialize(cordova, webView);
+		/* Get the telephony service from the native code, the context will be the cordova application
+		 * that is invoking the service
+		 */
+		this.Tel = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+		this.cellLocation = (GsmCellLocation) Tel.getCellLocation();
+	}
+	
+	/**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getSignalInfo")) {
+            JSONObject r = new JSONObject();
+            r.put("imei", this.getMobileIdentifier());
+            r.put("operator", this.getOperator());
+            r.put("cellID", this.getCellID());
+            r.put("lac", this.getLac());
+            r.put("neighbourInfo", this.getNeighbours());
+            callbackContext.success(r);
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+	
+    /*All the local methods to give results back to cordova app
+     * 1. getMobileIdentifier()
+     * 2. getOperator()
+     * 3. getCellID()
+     * 4. getLac()
+     * 5. getNeighbours()
+     */
+    
+    public String getMobileIdentifier(){
+    	return this.Tel.getDeviceId();
+    }
+    
+    public String getOperator(){
+    	return this.Tel.getNetworkOperatorName();
+    }
+    
+    public int getCellID(){
+    	return this.cellLocation.getCid();
+    }
+    
+    public int getLac(){
+    	return this.cellLocation.getLac();
+    }
+    
+    public JSONArray getNeighbours(){
+		this.setNeighboringList(this.Tel.getNeighboringCellInfo());
+    	JSONArray neighborsArray = new JSONArray();
+    	for(int i=0; i< getNeighboringList().size(); i++){
+    		JSONObject neighbors = new JSONObject();
+    		String dbM = null,
+    				cid = null,
+    				lac = null;
+    		cid = String.valueOf(getNeighboringList().get(i).getCid());
+    		lac = String.valueOf(getNeighboringList().get(i).getLac());
+    		int rssi = getNeighboringList().get(i).getRssi();
+    		if(rssi == NeighboringCellInfo.UNKNOWN_RSSI)
+    			dbM = "Unknown RSSI";
+    		else
+    			dbM = String.valueOf(-113 + 2 * rssi) + " dBm";
+    		try {
+				neighbors.put("rssi", dbM);
+				neighbors.put("cid", cid);
+	    		neighbors.put("lac", lac);
+	    		neighborsArray.put(neighbors);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	return neighborsArray;
+    }
+    
+
+	public static List<NeighboringCellInfo> getNeighboringList() {
+		return NeighboringList;
+	}
+
+	public void setNeighboringList(List<NeighboringCellInfo> neighboringList) {
+		NeighboringList = neighboringList;
+	}
+
+
+	private class MyPhoneStateListener extends PhoneStateListener {
+
+	}
+}
---
+++ www/signal.js
@@ -0,0 +1,71 @@
+/* Signal Custom Plugin 
+ *
+ *
+ *
+ */
+
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec'),
+    cordova = require('cordova');
+
+channel.createSticky('onCordovaInfoReady');
+// Tell cordova channel to wait on the CordovaInfoReady event
+channel.waitForInitialization('onCordovaInfoReady');
+
+/*
+ * Custom signal object to get and store the values of advanced signal information returned from native code
+ * Properties include:
+ * 1. IMEI
+ * 2. Operator Name
+ * 3. CellID latched to
+ * 4. LAC of the current CellID
+ * 5. Neighboring Cell Sites
+ */
+
+function Signal() {
+    this.available = false;
+    this.imei = null;
+    this.operator = null;
+    this.cellID = null;
+    this.lac = null;
+    this.neighbors = null;
+
+    var me = this;
+
+    channel.onCordovaReady.subscribe(function() {
+        me.getAdvancedNetworkInfo(function(info) {
+            me.available = true;
+            me.imei = info.imei;
+            me.operator = info.operator;
+            me.cellID = info.cellID;
+            me.lac = info.lac;
+            me.neighbors = info.neighbors;
+            channel.onCordovaInfoReady.fire();
+        }, function(e) {
+            me.available = false;
+            utils.alert("[ERROR] Error initializing Cordova: " + e);
+        });
+    });
+
+}
+
+/*Get the information using the getAdvancedNetworkInfo method defined above,
+ * use cordova.exec to initiate the native code underneath
+ */
+
+Signal.prototype.getAdvancedNetworkInfo = function(successCallback, errorCallback) {
+    argscheck.checkArgs('Network', 'Signal.getAdvancedNetworkInfo', arguments);
+    exec(null, null, 'Signal', 'getSignalInfo', []);
+};
+
+module.exports = new Signal();
+
+/*var signal = {
+    getSignalInfo: function() {
+        cordova.exec(null, null, 'Signal', 'getSignalInfo', []);
+    }
+}
+
+module.exports = signal;*/
